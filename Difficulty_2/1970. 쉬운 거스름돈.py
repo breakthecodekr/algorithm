c2cd77af291346bdb1052e17ca5b9a6bc68b7cd0
sngn-libby <44948368+sngn-libby@users.noreply.github.com>
2019-04-09 13:34:05 +0900

    Update 1970. 쉬운 거스름돈.py

--- Difficulty_2/1970. 쉬운 거스름돈.py
+++ Difficulty_2/1970. 쉬운 거스름돈.py
@@ -1,18 +1,24 @@
+# 거스름돈 단위를 list로 지정
 paper = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
 trial = range(int(input()))
 
 for j in trial:
-
+    
+    # 회차 출력
     j += 1
     print("#" + "{}".format(j))
-
+    
+    # 받은 돈 입력
     N = int(input())
     arr = []
     temp = 0
+    
+    # 거스름돈 계산
     for i in paper:
         temp = N % i
         N = N // i
         arr.append(str(N))
         N = temp
-
+    
+    # 거스름돈 
     print(' '.join(arr))
